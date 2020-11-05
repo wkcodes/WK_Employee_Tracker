63f2df68dfd427b3740b4ea32c331f40738f1b19
Will Kendall <will@wills-MacBook-Pro.local>
2020-11-05 11:37:58 -0800

    reconfigured schema and sql files

---
+++ config/index.js
@@ -0,0 +1,13 @@
+let connection = require("./connection");
+ 
+class employee_db{
+    constructor(connection){
+        this.connection = connection;
+    }
+}
+
+//make vs code change stuff in database
+//controllers add/remover/update 
+//two js folders: query to database
+//index with inquiere
+//queries with mysql
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO department(name) values("management");
+INSERT INTO department(name) values("boh");
+INSERT INTO department(name) values("foh");
+
+INSERT INTO role(title,salary,department_id) values("gm","70000",1);
+INSERT INTO role(title,salary,department_id) values("chef","50000",2);
+INSERT INTO role(title,salary,department_id) values("server","30000",3);
+
+--three for employee
