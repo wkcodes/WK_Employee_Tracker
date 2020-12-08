2eec5bb243d6ce37a865f55dec41c34142dea1d3
Will Kendall <will@wills-MacBook-Pro.local>
2020-12-07 21:16:33 -0800

    db mods

--- db/seed.sql
+++ db/seed.sql
@@ -6,4 +6,6 @@ INSERT INTO role(title,salary,department_id) values("gm","70000",1);
 INSERT INTO role(title,salary,department_id) values("chef","50000",2);
 INSERT INTO role(title,salary,department_id) values("server","30000",3);
 
---three for employee
+INSERT INTO employee(first_name,last_name,role_id,manager_id) values("Bob","Boberts",1,null);
+INSERT INTO employee(first_name,last_name,role_id,manager_id) values("Sharon","Stevenson",2,1);
+INSERT INTO employee(first_name,last_name,role_id,manager_id) values("Howard","Handley",3,1);
